04daa9950eb1310b0f539ec4a72dd7bb47b84732
David Van Der Beek <earlynovrock@gmail.com>
2017-02-17 11:18:05 -0600

    persist sessions

--- web/src/containers/App/index.js
+++ web/src/containers/App/index.js
@@ -1,16 +1,28 @@
 // @flow
 import React, { Component } from 'react';
 import { BrowserRouter, Match, Miss } from 'react-router';
+import { connect } from 'react-redux';
+import { authenticate } from '../../actions/session';
 import Home from '../Home';
 import NotFound from '../../components/NotFound';
 import Login from '../Login';
 import Signup from '../Signup';
 
 class App extends Component {
+  componentDidMount() {
+    const token = localStorage.getItem('token');
+
+    if (token) {
+      this.props.authenticate();
+    }
+  }
+
+  props: Props
+
   render() {
     return (
       <BrowserRouter>
-        <div>
+        <div style={{ display: 'flex', flex: '1' }}>
           <Match exactly pattern="/" component={Home} />
           <Match pattern="/login" component={Login} />
           <Match pattern="/signup" component={Signup} />
@@ -21,4 +33,7 @@ class App extends Component {
   }
 }
 
-export default App;
+export default connect(
+  null,
+  { authenticate }
+)(App);
